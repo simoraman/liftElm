cfa1cc7a5599a18222110823cb153c768fda28db
Simo Råman <simoraman@gmail.com>
2016-04-24 13:41:07 +0300

    Render model

--- src/index.elm
+++ src/index.elm
@@ -1,20 +1,24 @@
 import Html exposing (div, button, text, input, label)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import StartApp as StartApp
 import Effects exposing (Effects, Never)
+import Maybe exposing (map)
 
 app = StartApp.start { init = (model, Effects.none), view = view, update = update, inputs = [] }
 main = app.html
 type alias Set = { lift: String, reps: Int, weight: Float}
 model: List Set
-model = []
+model = [{lift = "Squat", reps = 5, weight = 50}, {lift = "Bench", reps = 5, weight = 50}]
 update a m = (m, a)
 
-view x model = 
-  div [] 
-    [ text "Squat", 
-      div [] [ 
-        label [] [ text "Reps", input [] [] ], 
-        label [] [ text "Weight", input [] [] ] 
-      ]
+liftTemplate: Set -> List Html.Html
+liftTemplate set = 
+  [ text set.lift, 
+    div [] [ 
+      label [] [ text "Reps", input [value (toString set.reps)] [] ], 
+      label [] [ text "Weight", input [value (toString set.weight)] [] ] 
     ]
+  ]
+view x model = 
+  div [] (List.concatMap liftTemplate model)
