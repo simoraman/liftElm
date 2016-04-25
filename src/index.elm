b51bd354f88479a4ba3cfb30e4a47ab52db6aed2
Simo Råman <simoraman@gmail.com>
2016-04-25 23:46:01 +0300

    Add some id

--- src/index.elm
+++ src/index.elm
@@ -7,28 +7,29 @@ import Maybe exposing (map)
 
 app = StartApp.start { init = (emptyModel, Effects.none), view = view, update = update, inputs = [] }
 main = app.html
-type alias Set = { id: Int,  lift: String, reps: Int, weight: Float}
+type alias Set = { id: Int, lift: String, reps: Int, weight: Float}
 
-emptySet = { id = 0, lift = "", reps = 0, weight = 0 }
-emptyModel = { lifts = [], newSet = emptySet }
+emptySet id = { id = id, lift = "", reps = 0, weight = 0 }
+emptyModel = { lifts = [], newSet = (emptySet 0), nextId = 1 }
 
 type Action = Insert Set
 
 update action model = 
   case action of 
-    Insert set -> ({model | lifts = ([set] ++ model.lifts)}, Effects.none)
+    Insert set -> ({model | lifts = ([set] ++ model.lifts), nextId = model.nextId + 1}, Effects.none)
   
 liftTemplate: Set -> List Html.Html
 liftTemplate set = 
   [  
-    div [] [ 
+    div [] [
+      text (toString set.id), 
       label [] [ text "Lift: ", input [value set.lift ] [] ], 
       label [] [ text "Reps: ", input [value (toString set.reps)] [] ], 
       label [] [ text "Weight: ", input [value (toString set.weight)] [] ] 
     ]
   ]
-newSetTemplate address = 
-    button [onClick address (Insert emptySet)] [ text "+" ] 
+newSetTemplate address model = 
+    button [onClick address (Insert (emptySet model.nextId))] [ text "+" ] 
   
 
 view address model = 
@@ -37,5 +38,5 @@ view address model =
       img [src "asset/bicep.png", align "middle"] [], 
       text "liftElm", 
       img [src "asset/bicep2.png", align "middle"] []]]
-    ++ [newSetTemplate address] 
+    ++ [newSetTemplate address model] 
     ++ (List.concatMap liftTemplate model.lifts))
