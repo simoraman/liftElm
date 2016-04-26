699fb6968a002d6386cc3eae3229c66e0651fde6
Simo Råman <simoraman@gmail.com>
2016-04-26 21:38:19 +0300

    Update items

--- src/index.elm
+++ src/index.elm
@@ -1,9 +1,10 @@
 import Html exposing (h1, img, div, button, text, input, label)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on, targetValue)
 import StartApp as StartApp
 import Effects exposing (Effects, Never)
 import Maybe exposing (map)
+import String exposing (..)
 
 app = StartApp.start { init = (emptyModel, Effects.none), view = view, update = update, inputs = [] }
 main = app.html
@@ -12,20 +13,59 @@ type alias Set = { id: Int, lift: String, reps: Int, weight: Float}
 emptySet id = { id = id, lift = "", reps = 0, weight = 0 }
 emptyModel = { lifts = [], newSet = (emptySet 0), nextId = 1 }
 
-type Action = Insert Set
+type Action = NoOp 
+  | Insert Set 
+  | UpdateLift Int String
+  | UpdateWeight Int Float
+  | UpdateReps Int Int
 
+updateLiftName model id name = 
+  let
+    updatedLifts = model.lifts 
+      |> List.map (\l -> if l.id == id then {l | lift = name} else l) 
+  in
+    { model | lifts = updatedLifts } 
+
+updateLiftWeight model id weight = 
+  let
+    updatedLifts = model.lifts 
+      |> List.map (\l -> if l.id == id then {l | weight = weight} else l) 
+  in
+    { model | lifts = updatedLifts } 
+
+updateLiftReps model id reps = 
+  let
+    updatedLifts = model.lifts 
+      |> List.map (\l -> if l.id == id then {l | reps = reps} else l) 
+  in
+    { model | lifts = updatedLifts } 
+    
 update action model = 
   case action of 
     Insert set -> ({model | lifts = ([set] ++ model.lifts), nextId = model.nextId + 1}, Effects.none)
-  
-liftTemplate: Set -> List Html.Html
-liftTemplate set = 
+    UpdateLift id str -> (updateLiftName model id str, Effects.none)
+    UpdateWeight id weight -> (updateLiftWeight model id weight, Effects.none)
+    UpdateReps id reps -> (updateLiftReps model id reps, Effects.none)
+    _ -> (model, Effects.none)
+    
+toIntAction action str =
+  case String.toInt str of 
+    Result.Ok i -> action i
+    _ -> NoOp 
+ 
+toFloatAction action str =
+  case String.toFloat str of 
+    Result.Ok i -> action i
+    _ -> NoOp
+         
+liftTemplate: Signal.Address Action -> Set -> List Html.Html
+liftTemplate address set = 
   [  
     div [] [
       text (toString set.id), 
-      label [] [ text "Lift: ", input [value set.lift ] [] ], 
-      label [] [ text "Reps: ", input [value (toString set.reps)] [] ], 
-      label [] [ text "Weight: ", input [value (toString set.weight)] [] ] 
+      label [] [ text "Lift: ", input [value set.lift, on "input" targetValue (\str -> Signal.message address (UpdateLift set.id str))] [] ], 
+      label [] [ text "Reps: ", input [value (toString set.reps), on "input" targetValue (\str -> Signal.message address (toIntAction (UpdateReps set.id) str))] [] ], 
+      label [] [ text "Weight: ", input [value (toString set.weight), on "input" targetValue (\str -> Signal.message address (toFloatAction (UpdateWeight set.id) str))] [] ] 
     ]
   ]
 newSetTemplate address model = 
@@ -39,4 +79,4 @@ view address model =
       text "liftElm", 
       img [src "asset/bicep2.png", align "middle"] []]]
     ++ [newSetTemplate address model] 
-    ++ (List.concatMap liftTemplate model.lifts))
+    ++ (List.concatMap (liftTemplate address) model.lifts))
